2eb414a54876edc72c4129db6438eab0f38b66ed
Sharad <sharad.rajore@gmail.com>
2021-04-15 11:23:00 +0530

    initial commit

---
+++ src/main/java/com/zensar/sampleappweb/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.zensar.sampleappweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController  {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "<h1>Welcome to Jenkins </h1>";
+	}
+	
+	@RequestMapping("/hi")
+	public String sayHi() {
+		return "<h1>Welcome to Jenkins and saying Hi </h1>";
+	}
+
+}
